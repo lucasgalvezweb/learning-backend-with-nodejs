eea06f176724477b9ce69896351dc330c892bda6
Lucas Galvez <lgalvez@utec.edu.pe>
2025-08-04 22:56:55 -0500

    noc best practices

---
+++ 05-noc/src/infrastructure/datasources/file-system.datasource.ts
@@ -0,0 +1,67 @@
+import fs from 'fs';
+
+import { LogDataSource } from "../../domain/datasources/log.datasource";
+import { LogEntity, LogSeverityLevel } from "../../domain/entities/log.entity";
+
+export class FileSystemDatasource implements LogDataSource {
+    private readonly logPath = 'logs/';
+    private readonly allLogsPath= 'logs/logs-all.log';
+    private readonly mediumLogsPath = 'logs/logs-medium.log';
+    private readonly highLogsPath = 'logs/logs-high.log';
+
+    constructor() {
+        this.createLogsFiles();
+    }
+
+    private createLogsFiles = () => {
+        if (!fs.existsSync(this.logPath)) {
+            fs.mkdirSync(this.logPath);
+        }
+
+        [
+            this.allLogsPath,
+            this.mediumLogsPath,
+            this.highLogsPath
+        ].forEach(path => {
+            if (fs.existsSync(path)) return;
+            fs.writeFileSync(path, '');
+        })
+    }
+
+    async saveLog(newLog: LogEntity): Promise<void> {
+
+        const logAsJson = `${JSON.stringify(newLog)}\n`;
+        
+        fs.appendFileSync( this.allLogsPath, logAsJson);
+
+        // If the log is low, do nothing
+        if (newLog.level === LogSeverityLevel.low) return;
+
+        // If the log is medium or high, append to the respective file
+        if (newLog.level === LogSeverityLevel.medium) {
+            fs.appendFileSync(this.mediumLogsPath, logAsJson);
+        } else {
+            fs.appendFileSync(this.highLogsPath, logAsJson);
+        }
+    }
+
+    private getLogsFromFile = (path: string): LogEntity[] => {
+        const content = fs.readFileSync(path, 'utf-8');
+        const logs = content.split('\n').map(LogEntity.fromJson);
+        return logs;
+    }
+
+    getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
+        
+        switch (severityLevel) {
+            case LogSeverityLevel.low:
+                return this.getLogsFromFile(this.allLogsPath);
+            case LogSeverityLevel.medium:
+                return this.getLogsFromFile(this.mediumLogsPath);
+            case LogSeverityLevel.high:
+                return this.getLogsFromFile(this.highLogsPath);
+            default:
+                throw new Error(`Unknown severity level: ${severityLevel}`);
+        }
+    }
+}
